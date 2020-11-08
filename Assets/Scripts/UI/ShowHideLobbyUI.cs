aef20564bcf12858a9bef3ebf4c23b1654a42ccf
Nintoryan <thehappystudio4@gmail.com>
2020-11-09 02:26:16 +0300

    Created Lobby

---
+++ Assets/Scripts/UI/ShowHideLobbyUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ShowHideLobbyUI : MonoBehaviour
+{
+    public GameObject LobbyUI;
+    private bool isHidden;
+    public void ShowHide()
+    {
+        LobbyUI.SetActive(isHidden);
+        isHidden = !isHidden;
+    }
+}

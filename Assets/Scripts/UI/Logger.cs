0d096ac0e899a93d7635ddd5624170d1181ef3b8
Nintoryan <thehappystudio4@gmail.com>
2020-11-09 04:22:06 +0300

    Начало UI lobby

---
+++ Assets/Scripts/UI/Logger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class Logger : MonoBehaviour
+{
+    private TMP_Text _log;
+    public static Logger Instance;
+    private List<string> _Logs = new List<string>();
+    [SerializeField] private int LogsSize = 4;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            _log = GetComponentInChildren<TMP_Text>();
+        }
+        else
+        {
+            if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    
+    
+    
+    public void Log(object LogData)
+    {
+        Debug.Log(LogData.ToString());
+        _Logs.Add(LogData.ToString());
+        if (_Logs.Count >= LogsSize)
+        {
+            _Logs.RemoveAt(0);
+        }
+        var fullLog = _Logs.Aggregate("", (current, log) => current + (log + "\n"));
+        _log.text = fullLog;
+    }
+}

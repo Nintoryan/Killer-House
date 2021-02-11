9257c6e6354ce3a0cbb5eada28f97d09396edb34
Nintoryan <thehappystudio4@gmail.com>
2021-02-11 03:04:18 +0300

    магазин

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private RectTransform ShopPanel;
+    [SerializeField] private RectTransform MainPanel;
+
+    public void OpenShopPanel()
+    {
+        ShopPanel.DOAnchorPosX(0, 0.5f);
+        MainPanel.DOAnchorPosX(-3000, 0.5f);
+    }
+
+    public void CloseShopPanel()
+    {
+        ShopPanel.DOAnchorPosX(3000, 0.5f);
+        MainPanel.DOAnchorPosX(0, 0.5f);
+    }
+}
---
+++ Assets/Scripts/UI/Shop/PreviewerRotation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PreviewerRotation : MonoBehaviour 
+{
+    [SerializeField] private float _sensitivity;
+    private Vector3 _mouseReference;
+    private Vector3 _mouseOffset;
+    private Vector3 _rotation;
+    private bool _isRotating;
+
+    private void Start ()
+    {
+        _rotation = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (!_isRotating) return;
+        
+        _mouseOffset = (Input.mousePosition - _mouseReference);
+        _rotation.y = -(_mouseOffset.x + _mouseOffset.y) * _sensitivity;
+        transform.Rotate(_rotation);
+        _mouseReference = Input.mousePosition;
+    }
+     
+    public void MouseDown()
+    {
+        _isRotating = true;
+        _mouseReference = Input.mousePosition;
+    }
+     
+    public void MouseUp()
+    {
+        _isRotating = false;
+    }
+     
+}
